4abd7a9990f6b5ebe618496282ae530c11cb4d57
itschool <itschool@tou.edu.kz>
2026-04-19 12:00:35 +0500

    122455

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use App\Http\Controllers\ArticleController;
 */
 
 Route::get("/articles", [ArticleController::class, "index"]);
+Route::get("/articles/{article}", [ArticleController::class, "show"]);
